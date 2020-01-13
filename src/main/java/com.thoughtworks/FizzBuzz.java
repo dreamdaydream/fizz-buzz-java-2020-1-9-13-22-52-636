9cd60028507cee9c42a73ee4d7e47f3d84f49f4d
linlifu2 <linlin.fu@thoughtworks.com>
2020-01-13 11:52:46 +0800

    add contains about 3,5,7

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -3,16 +3,24 @@ package com.thoughtworks;
 public class FizzBuzz {
 
   public String fizzBuzz(Integer number) {
+    String stringNumber = number.toString();
+    Boolean isContain3 = stringNumber.contains("3");
+    Boolean isContain5 = stringNumber.contains("5");
+    Boolean isContain7 = stringNumber.contains("7");
+
     String result  = "";
-    if (number % 3 == 0) {
+    if (isContain3 && !isContain5) {
+      return "Fizz";
+    }
+    if (number % 3 == 0 && (!isContain5 || isContain7)) {
       result += "Fizz";
     }
-    if (number % 5 == 0) {
+    if (number % 5 == 0 && !isContain7) {
       result += "Buzz";
     }
     if (number % 7 == 0) {
       result += "Whizz";
     }
-    return result.isEmpty()? number.toString(): result;
+    return result.isEmpty()? stringNumber: result;
   }
 }
--- src/test/java/com/thoughtworks/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -16,10 +16,30 @@ public class FizzBuzzTest {
   @Test
   public void should_return_combine_result_given_the_number_is_multiple_divided_by_3_5_and_7() {
     FizzBuzz fizzBuzz = new FizzBuzz();
-    assertEquals("FizzBuzz", fizzBuzz.fizzBuzz(15));
+    assertEquals("FizzBuzz", fizzBuzz.fizzBuzz(90));
     assertEquals("FizzWhizz", fizzBuzz.fizzBuzz(21));
-    assertEquals("BuzzWhizz", fizzBuzz.fizzBuzz(35));
-    assertEquals("FizzBuzzWhizz", fizzBuzz.fizzBuzz(105));
+    assertEquals("BuzzWhizz", fizzBuzz.fizzBuzz(105));
+  }
 
+  @Test
+  public void should_return_Fizz_given_a_number_is_contain_3() {
+    FizzBuzz fizzBuzz = new FizzBuzz();
+    assertEquals("Fizz", fizzBuzz.fizzBuzz(13));
+    assertEquals("Fizz", fizzBuzz.fizzBuzz(30));
+    assertEquals("Fizz", fizzBuzz.fizzBuzz(63));
+  }
+
+  @Test
+  public void should_return_Buzz_given_a_number_contains_5() {
+    FizzBuzz fizzBuzz = new FizzBuzz();
+    assertEquals("Buzz", fizzBuzz.fizzBuzz(15));
+    assertEquals("BuzzWhizz", fizzBuzz.fizzBuzz(35));
+  }
+
+  @Test
+  public void should_return_Whizz_given_a_number_contains_7() {
+    FizzBuzz fizzBuzz = new FizzBuzz();
+    assertEquals("Fizz", fizzBuzz.fizzBuzz(27));
+    assertEquals("Whizz", fizzBuzz.fizzBuzz(70));
   }
 }
